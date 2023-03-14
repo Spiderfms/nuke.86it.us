7b2d0d7fd29b729d8136f8895c7428620dd39949
Ernest Buffington <ernest.buffington@gmail.com>
2023-03-14 12:47:18 -0400

    Update mysqli.php

--- html/db/mysqli.php
+++ html/db/mysqli.php
@@ -23,19 +23,19 @@ if(!defined("SQL_LAYER"))
 {
 
   if(!defined('SQL_LAYER')){
-  define("SQL_LAYER","mysqli");
+    define("SQL_LAYER","mysqli");
   }
   if(!defined('MYSQL_NUM')){
-  define("SQL_NUM", "MYSQL_NUM");
+    define("SQL_NUM", "MYSQL_NUM");
   }
   if(!defined('MYSQL_BOTH')){
-  define("SQL_BOTH", "MYSQL_BOTH");
+    define("SQL_BOTH", "MYSQL_BOTH");
   }
   if(!defined('MYSQL_ASSOC')){
-  define("SQL_ASSOC", "MYSQL_ASSOC");
+    define("SQL_ASSOC", "MYSQL_ASSOC");
   }
   if(!defined('END_TRANSACTION')){
-  define('END_TRANSACTION', "2");
+    define('END_TRANSACTION', "2");
   }
 
 class sql_db
