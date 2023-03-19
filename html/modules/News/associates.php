7d8de545e2298f9a843b5611e7c980827417cdd1
Ernest Buffington <ernest.buffington@gmail.com>
2023-03-19 13:08:10 -0400

    Update associates.php

--- html/modules/News/associates.php
+++ html/modules/News/associates.php
@@ -1,5 +1,5 @@
 <?php
-if(!strpos($_SERVER['PHP_SELF'], 'admin.php')) {
+if(!strpos((string) $_SERVER['PHP_SELF'], 'admin.php')) {
 	#show right panel:
 	define('INDEX_FILE', true);
 }
@@ -15,6 +15,11 @@ if(!strpos($_SERVER['PHP_SELF'], 'admin.php')) {
 /* the Free Software Foundation; either version 2 of the License.       */
 /************************************************************************/
 
+/* Applied rules:
+ * ListToArrayDestructRector (https://wiki.php.net/rfc/short_list_syntax https://www.php.net/manual/en/migration71.new-features.php#migration71.new-features.symmetric-array-destructuring)
+ * NullToStrictStringFuncCallArgRector
+ */
+ 
 if (!defined('MODULE_FILE')) {
 	die("You can't access this file directly...");
 }
@@ -24,16 +29,16 @@ $module_name = basename(dirname(__FILE__));
 get_lang($module_name);
 $sid = intval($sid);
 $query = $db->sql_query("SELECT associated FROM ".$prefix."_stories WHERE sid='$sid'");
-list($associated) = $db->sql_fetchrow($query);
+[$associated] = $db->sql_fetchrow($query);
 
 if (!empty($associated)) {
 	OpenTable();
 	echo "<center><b>"._ASSOTOPIC."</b><br><br>";
-	$asso_t = explode("-",$associated);
+	$asso_t = explode("-",(string) $associated);
 	for ($i=0; $i<sizeof($asso_t); $i++) {
 		if (!empty($asso_t[$i])) {
 		        $query = $db->sql_query("SELECT topicimage, topictext from ".$prefix."_topics WHERE topicid='".$asso_t[$i]."'");
-			list($topicimage, $topictext) = $db->sql_fetchrow($query);
+			[$topicimage, $topictext] = $db->sql_fetchrow($query);
 			echo "<a href=\"modules.php?name=$module_name&new_topic=$asso_t[$i]\"><img src=\"".$tipath."/".$topicimage."\" border=\"0\" hspace=\"10\" alt=\"".$topictext."\" title=\"".$topictext."\"></a>";
 		}
 	}
